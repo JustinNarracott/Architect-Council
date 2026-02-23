42e1e561a0e3232a0faa9df2965c436a03839b21
JustinNarracott <justin.narracott@step5group.com>
2026-02-23 17:19:06 +0000

    fix: export create_codebase_crew from crews __init__

--- backend/crews/__init__.py
+++ backend/crews/__init__.py
@@ -1,3 +1,4 @@
 from .architecture_crew import create_architecture_crew, run_evaluation
+from .codebase_crew import create_codebase_crew
 
-__all__ = ["create_architecture_crew", "run_evaluation"]
+__all__ = ["create_architecture_crew", "run_evaluation", "create_codebase_crew"]
